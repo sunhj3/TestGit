e814a91da42e56efeb03c60b1db5cd8c74098452
shj_global <shj_global@asiainfo.com>
2018-10-21 16:25:07 +0800

    123456

---
+++ src/main/java/com/shj/git/Apple.java
@@ -0,0 +1,10 @@
+package com.shj.git;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		System.out.println("123456");
+
+	}
+
+}
---
+++ src/main/java/com/shj/git/Happy.java
@@ -0,0 +1,7 @@
+package com.shj.git;
+
+public class Happy {
+	public static void main(String[] args) {
+		System.out.println("123456");
+	}
+}
